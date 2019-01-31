33cb22a7b732c050e369dfa7148466cea8b66923
Kye Berkeland <kyeberkeland@yahoo.com>
2019-01-31 13:58:13 -0600

    Add class structure

---
+++ src/components/Favorites/FavoriteItems.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class FavoriteItems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            testArray: [],
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FavoriteItems;
